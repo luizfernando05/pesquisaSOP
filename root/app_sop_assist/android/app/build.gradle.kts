426567caaa3368b900f0c93d1915e8698aed4341
Wesley <wesleycontatopessoal7@gmail.com>
2025-05-06 15:26:17 -0300

    Ajustando .ndkVersion
    Nova versão

--- root/app_sop_assist/android/app/build.gradle.kts
+++ root/app_sop_assist/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.app_sop_assist"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
