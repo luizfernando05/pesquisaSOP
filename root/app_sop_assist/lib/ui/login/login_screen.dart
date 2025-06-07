36d55e0e1cc4638f519a35a4ae0f3f65c87d71a2
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2025-06-07 12:59:46 -0300

    adicionando comentário para ignorar uso de BuildContext de forma assíncrona

--- root/app_sop_assist/lib/ui/login/login_screen.dart
+++ root/app_sop_assist/lib/ui/login/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:app_sop_assist/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
